1407afbcebef4696016c9f3f2f8364e54de2cbaa
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-10-19 23:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+  <script>
+    let count = 0;
+    const counter = document.getElementById("counter");
+    const btn = document.getElementById("incrementBtn");
+
+    btn.addEventListener("click", function() {
+      alert("Current (un-incremented) value: " + count);
+      count++;
+      counter.textContent = count;
+    });
+  </script>
